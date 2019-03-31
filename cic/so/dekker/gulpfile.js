a5f3dad69f2b55793df5ca1a6bfe5cea6aed9800
asm95 <crissonicx2@gmail.com>
2019-03-31 13:44:49 -0300

    [devops] compress html
    The new command will minify the HTML in order to reduce transfer
    traffic.

--- cic/so/dekker/gulpfile.js
+++ cic/so/dekker/gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
+const rename = require("gulp-rename");
 const zip = require('gulp-zip');
 const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
 
-gulp.task('compress', function () {
+
+gulp.task('compress-css', function () {
   return gulp.src('simulador.js')
     .pipe( uglify({mangle:{properties:{keep_quoted: true, reserved: [
         'ready', 'addClass', 'removeClass', 'each', 'text', 'val', 'on', 'attr', 'html',
@@ -10,13 +13,27 @@ gulp.task('compress', function () {
         'stringify', 'parse',
         'highlightBlock',
         ]}}}) )
-    .pipe( gulp.dest('pack') );
+    .pipe( rename('simulador.min.js') )
+    .pipe( gulp.dest('dist') );
+});
+
+gulp.task('compress-html', function () {
+    return gulp.src('simulador.html')
+    .pipe( htmlmin({
+        collapseBooleanAttributes: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        }))
+    .pipe( rename('simulador.html') )
+    .pipe( gulp.dest('dist') );
 });
 
 gulp.task('zip', function () {
-    return gulp.src(['./pack/**', './static/**', 'simulador.html'], {base: '.'})
+    return gulp.src(['./dist/**', './static/**'], {base: '.'})
         .pipe(zip('dist-app.zip'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task('default', gulp.series('compress'));
+gulp.task('default', gulp.series('compress-css', 'compress-html'));
